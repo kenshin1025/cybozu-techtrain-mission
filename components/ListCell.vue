29b71e24a2ba9bb5059e24bca064a0f6ff2320fe
18mac-01 <18mac-01@pc18mac-01.local>
2020-03-05 21:19:03 +0900

    一覧表示を長い文字列の時切り捨てるようにした

--- components/ListCell.vue
+++ components/ListCell.vue
@@ -1,9 +1,18 @@
 <template>
+  <!-- <v-card :href="company.url" target="_blank"> -->
   <v-card>
-    <v-card-title class="headline">{{company.name}}</v-card-title>
-    <v-card-text>{{company.summary}}</v-card-text>
+    <v-card-title class="headline">
+      <a :href="company.url" target="_blank" class="text-truncate">{{company.name}}</a>
+    </v-card-title>
+    <v-card-text class="text-truncate">{{company.summary}}</v-card-text>
   </v-card>
 </template>
+
+<style scoped>
+a {
+  color: black;
+}
+</style>
 
 <script>
 export default {
@@ -13,7 +22,12 @@ export default {
     };
   },
   props: {
-      company: Object
+    company: Object
+  },
+  methods: {
+    openCompanyPage() {
+      window.open(this.company.url, "google");
+    }
   }
 };
 </script>
